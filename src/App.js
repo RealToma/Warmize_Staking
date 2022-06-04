b0f70e13592504466c45d8d0667056b6a062ae22
thomas.k98114@gmail.com <thomas.k98114@gmail.com>
2022-06-04 18:53:45 -0400

    fixed issues02

--- src/App.js
+++ src/App.js
@@ -34,11 +34,10 @@ function App() {
   const { activate, library, active } = useWeb3React();
 
   const handleConnect = async (currentConnector) => {
-    // await activate(walletConnectors[currentConnector]);
+    await activate(walletConnectors[currentConnector]);
     activate(walletConnectors[currentConnector]);
-
     window.localStorage.setItem(CURRENT_WALLET_STORAGE_NAME, currentConnector);
-    // handleSwitch();
+    handleSwitch();
     handleClose();
     setConnected(true);
   }
@@ -61,21 +60,21 @@ function App() {
   }
 
   useEffect(() => {
-    // const currentConnect = window.localStorage.getItem(CURRENT_WALLET_STORAGE_NAME);
-    // if (currentConnect) {
-    //   if (!active) {
-    //     activate(walletConnectors[currentConnect]).then(() => {
-    //       if (parseInt(window.ethereum.chainId) !== chainId) {
-    //         let msg = "You are in " + chainIds[parseInt(window.ethereum.chainId)] +
-    //           " network. Plz switch to " + NETWORK_NAME;
-    //         NotificationManager.error(msg, "ERROR", 3000);
-    //         setConnected(false);
-    //         console.log(msg);
-    //       }
-    //       else setConnected(true);
-    //     })
-    //   }
-    // }
+    const currentConnect = window.localStorage.getItem(CURRENT_WALLET_STORAGE_NAME);
+    if (currentConnect) {
+      if (!active) {
+        activate(walletConnectors[currentConnect]).then(() => {
+          if (parseInt(window.ethereum.chainId) !== chainId) {
+            let msg = "You are in " + chainIds[parseInt(window.ethereum.chainId)] +
+              " network. Plz switch to " + NETWORK_NAME;
+            NotificationManager.error(msg, "ERROR", 3000);
+            setConnected(false);
+            console.log(msg);
+          }
+          else setConnected(true);
+        })
+      }
+    }
   }, [library, active])
 
   useEffect(() => {
@@ -84,18 +83,18 @@ function App() {
 
   return (
     <>
-      <StyledComponent>
-        <Topbar
-          current={current}
-          active={active}
-          setConnected={setConnected}
-          setCurrent={setCurrent}
-          handleConnect={handleConnect}
-        // handleSwitch={handleSwitch}
-        />
-        <Content />
-        <NotificationContainer />
-      </StyledComponent>
+        <StyledComponent>
+          <Topbar
+            current={current}
+            active={active}
+            setConnected={setConnected}
+            setCurrent={setCurrent}
+            handleConnect={handleConnect}
+            handleSwitch={handleSwitch}
+          />
+          <Content />
+          <NotificationContainer />
+        </StyledComponent>
     </>
   );
 }
